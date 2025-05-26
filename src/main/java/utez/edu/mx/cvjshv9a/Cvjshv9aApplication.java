abe0000046abca1559a96df73231c50ef60c6878
JanethHdez1 <20223tn061@utez.edu.mx>
2025-05-26 17:47:53 -0600

    [FIRST_COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvjshv9a/Cvjshv9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvjshv9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvjshv9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvjshv9aApplication.class, args);
+    }
+
+}
